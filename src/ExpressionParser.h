c5ed9e96a77ee7d1275ae3d4e047457d172d5e03
m379h565 <143631627+m379h565@users.noreply.github.com>
2023-11-19 17:03:55 -0600

    ExpressionParser.h

--- src/ExpressionParser.h
+++ src/ExpressionParser.h
@@ -1,14 +1,12 @@
-// ExpressionParser.h
 #pragma once
 
 #include <memory>
 #include <stdexcept>
 #include "ASTNode.h"
 
-// ExpressionParser class for parsing algebraic expressions.
 class ExpressionParser {
 private:
-    const std::string expression;  // Changed from reference to object
+    const std::string expression;
     size_t pos;
     char getNextToken();
     std::unique_ptr<ASTNode> parseNumber();
