3551a0657b0c0ec71e85e1a27732794f080d5051
m379h565 <143631627+m379h565@users.noreply.github.com>
2023-11-19 03:06:04 -0600

    Update ExpressionParser.cpp (added parentheses function)

--- src/ExpressionParser.cpp
+++ src/ExpressionParser.cpp
@@ -1,10 +1,10 @@
 // ExpressionParser.cpp
 #include "ExpressionParser.h"
 #include <cctype>
+#include <cmath> 
 
 ExpressionParser::ExpressionParser(const std::string& expression) : expression(expression), pos(0) {}
 
-//call this after the parser gets to the left number of the AST to get the operator
 char ExpressionParser::getNextToken() {
     while (pos < expression.size() && std::isspace(expression[pos])) {
         ++pos;  // Skip whitespaces
@@ -13,49 +13,74 @@ char ExpressionParser::getNextToken() {
         return '\0';  // End of expression
     }
     return expression[pos++];
-    }
+}
 
-std::unique_ptr<ASTNode> ExpressionParser::parseNumber()  {
+std::unique_ptr<ASTNode> ExpressionParser::createBinaryOperationNode(char op, std::unique_ptr<ASTNode> left, std::unique_ptr<ASTNode> right) {
+    return std::make_unique<BinaryOperationNode>(op, std::move(left), std::move(right));
+}
+
+std::unique_ptr<ASTNode> ExpressionParser::parseNumber() {
     size_t startPos = pos;
-    while (pos < expression.size() && (std::isdigit(expression[pos]) || expression[pos] == '.')) { //this while statement might be able to be simplified
+    while (pos < expression.size() && (std::isdigit(expression[pos]) || expression[pos] == '.')) {
         ++pos;
     }
     std::string numStr = expression.substr(startPos, pos - startPos);
     return std::make_unique<NumberNode>(std::stod(numStr));
+}
+
+std::unique_ptr<ASTNode> ExpressionParser::parseFactor() {
+    char currentToken = getNextToken();
+
+    if (currentToken == '(') {
+        auto expressionInsideParentheses = parseExpression();
+        // Ensure the next token is the closing parenthesis
+        if (getNextToken() != ')') {
+            // Handle error: Missing closing parenthesis
+            // You can throw an exception or return an error node here
+        }
+        return expressionInsideParentheses;
+    } else {
+        pos--; // Put back the token if it's not an opening parenthesis
+        return parseNumber();
     }
+}
 
 std::unique_ptr<ASTNode> ExpressionParser::parseTerm() {
-    /*parseTerm is for whatever is being added or subtracted together and
-    parseFactor can be used for whatever is being multiplied and divided.
-    The AST will evaluate everything in the order it is created but it is kind of backwards
-    parseTerm is called first becuase the addition and subtraction will be evaluated last*/
-    //auto left = parseFactor();
-    auto left = parseNumber();
+    auto left = parseFactor();
+
+    while (true) {
+        char op = getNextToken();
+        if (op != '*' && op != '/' && op != '%' && op != '^') {
+            pos--; // Put back the token if it's not one of the recognized operators
+            break;
+        }
+        auto right = parseFactor();
+        switch (op) {
+            case '*':
+            case '/':
+            case '%':
+            case '^':
+                left = createBinaryOperationNode(op, std::move(left), std::move(right));
+                break;
+        }
+    }
+    return left;
+}
+
+std::unique_ptr<ASTNode> ExpressionParser::parseExpression() {
+    auto left = parseTerm();
     while (true) {
         char op = getNextToken();
         if (op != '+' && op != '-') {
-            pos--;  // Put back the token if it's not a '+' or '-'
+            pos--; // Put back the token if it's not a '+' or '-'
             break;
         }
-        //auto right = parseFactor();
-        auto right = parseNumber();
-        left = std::make_unique<BinaryOperationNode>(op, std::move(left), std::move(right));
-        }
+        auto right = parseTerm();
+        left = createBinaryOperationNode(op, std::move(left), std::move(right));
+    }
     return left;
-    }
-
-/*
-    std::unique_ptr<ASTNode> ExpressionParser::parseFactor() {
-        return left;
-    }
-*/
-
-/*
-will lastly have to create a similar function to parseFactor and parseTerm 
-that first check for parentheses and do all of the operations inside the parentheses in the order of operations
-before evaluating everything else
-*/
+}
 
 std::unique_ptr<ASTNode> ExpressionParser::parse() {
-    return parseTerm();
+    return parseExpression();
 }
