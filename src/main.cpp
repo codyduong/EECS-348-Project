b9cccc07fa2a6853af911668e1412de3f026ca4d
Cody Duong <duongcody507@gmail.com>
2023-11-26 20:02:54 -0600

    add versioning and help flag

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
 
 #include "ExpressionParser.h"
+#include "version.hpp"
 
-int main() {
+int main(int argc, char* argv[]) {
+    if (argc > 1) {
+        std::string arg = argv[1];
+
+        // Check for help or version flags
+        if (arg == "-h" || arg == "--help") {
+            std::cout << "Help message here..." << std::endl;
+            return 0;
+        } else if (arg == "-v" || arg == "--version") {
+            std::cout << "Version " << PROJECT_VERSION_MAJOR << "." << PROJECT_VERSION_MINOR << "."
+                      << PROJECT_VERSION_PATCH;
+
+            if (PROJECT_VERSION_RC > 0) {
+                std::cout << "-rc" << PROJECT_VERSION_RC;
+            }
+
+            std::cout << std::endl;
+            return 0;
+        }
+    }
+
     std::cout << "Enter an algebraic expression: ";
     std::string inputExpression;
     std::getline(std::cin, inputExpression);
